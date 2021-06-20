1458fbe87e5f1aae81a7be51f1264a81c4103429
Onur ARICI <oarici@suntek.io>
2021-06-20 21:59:00 +0300

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About me</h1>
+      <Link to="/">Back to home</Link>
+      <p>Hi there? I'm proud creator of this website, which i built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
